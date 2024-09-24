7b136303103284a0cdf12879e67bb2f2d77a323a
ShouZongChang <szc807414589@gmail.com>
2024-09-24 15:28:10 +0800

    chore(config): 配置

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,5 +6,17 @@ module.exports = {
     env: {
       NODE_ENV: "production",
     },
-  }]
+  }],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '110.42.138.166',
+      ref: 'origin/master',
+      repo: 'git@github.com:szc807414589/my-react-app.git',
+      path: '/var/www/my-react-app',
+      'pre-deploy-local': '',
+      'post-deploy': 'yarn && yarn build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
 };
